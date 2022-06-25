ae1cfddf1b98fcf00b7f02452a11fe76997ec7ab
nbur4556 <nbur4556@gmail.com>
2022-06-25 14:49:33 -0500

    feat: add current landing page content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,10 @@
 <main>
   <h1>Hi, I'm Nick and I am a Full Stack Web Developer</h1>
 
-  <section>
-    <h2>Very Brief About Me</h2>
-
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sequi voluptates
-      ducimus nulla sint dicta provident facere, deleniti temporibus repellat reprehenderit error
-      quidem explicabo suscipit quam. Veniam illum non deleniti!
-    </p>
-  </section>
+  <p>
+    I am a full stack developer based in Austin Texas. I have a passion in creating beautiful and
+    responsive web applications powered by scalable servers and databases.
+  </p>
 
   <section>
     <h2>Featured Portfolio Project(s)</h2>
@@ -17,5 +12,11 @@
 
   <section>
     <h2>Contact Me</h2>
+
+    <ul>
+      <li><a href="mailto: nbur4556@gmail.com">nbur4556@gmail.com</a></li>
+      <li><a href="https://github.com/nbur4556">Github</a></li>
+      <li><a href="https://www.linkedin.com/in/nick-burt/">Linked In</a></li>
+    </ul>
   </section>
 </main>
