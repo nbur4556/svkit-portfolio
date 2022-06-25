87bf33083bab26d12e39a5eee314ee74df387fd0
nbur4556 <nbur4556@gmail.com>
2022-06-25 16:33:05 -0500

    style: landing page spacing and sectioning

---
+++ src/components/Card.svelte
@@ -0,0 +1,3 @@
+<section class="p-4 bg-slate-200">
+  <slot />
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,40 +1,46 @@
 <script lang="ts">
+  import Card from "../components/Card.svelte";
   import ProjectDisplay from "../components/ProjectDisplay.svelte";
   import WorkExperience from "../components/WorkExperience.svelte";
+
   import { projects } from "../stores/projectStore";
 </script>
 
-<main class="h-screen">
-  <h1>Hi, I'm Nick and I am a Full Stack Web Developer</h1>
+<main>
+  <section class="h-screen flex-col space-y-4 p-4">
+    <Card>
+      <h1>Hi, I'm Nick and I am a Full Stack Web Developer</h1>
 
-  <p>
-    I am a full stack developer based in Austin Texas. I have a passion in creating beautiful and
-    responsive web applications powered by scalable servers and databases.
-  </p>
+      <p>
+        I am a full stack developer based in Austin Texas. I have a passion in creating beautiful
+        and responsive web applications powered by scalable servers and databases.
+      </p>
+    </Card>
 
-  <section>
-    <h2>Featured Portfolio Project(s)</h2>
+    <Card>
+      <h2>Featured Portfolio Project(s)</h2>
 
-    {#each $projects as project, index}
-      <ProjectDisplay item={project} />
-    {/each}
-  </section>
+      {#each $projects as project, index}
+        <ProjectDisplay item={project} />
+      {/each}
+    </Card>
 
-  <section>
-    <h2>Work Experience</h2>
+    <Card>
+      <h2>Work Experience</h2>
 
-    <WorkExperience title="Melodix">
-      Developing a full-stack web application for local musicians.
-    </WorkExperience>
-  </section>
+      <WorkExperience title="Melodix">
+        Developing a full-stack web application for local musicians.
+      </WorkExperience>
+    </Card>
 
-  <section>
-    <h2>Contact Me</h2>
+    <Card>
+      <h2>Contact Me</h2>
 
-    <ul>
-      <li><a href="mailto: nbur4556@gmail.com" target="_blank">nbur4556@gmail.com</a></li>
-      <li><a href="https://github.com/nbur4556" target="_blank">Github</a></li>
-      <li><a href="https://www.linkedin.com/in/nick-burt/" target="_blank">Linked In</a></li>
-    </ul>
+      <ul>
+        <li><a href="mailto: nbur4556@gmail.com" target="_blank">nbur4556@gmail.com</a></li>
+        <li><a href="https://github.com/nbur4556" target="_blank">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/nick-burt/" target="_blank">Linked In</a></li>
+      </ul>
+    </Card>
   </section>
 </main>
