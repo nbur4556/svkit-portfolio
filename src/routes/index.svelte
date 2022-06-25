3a902f3672025804aab0a8bd39523c7a188d34cb
nbur4556 <nbur4556@gmail.com>
2022-06-25 14:01:26 -0500

    feat: initial content and structure for landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,21 @@
 <main>
-  <h1>Hello World</h1>
+  <h1>Hi, I'm Nick and I am a Full Stack Web Developer</h1>
+
+  <section>
+    <h2>Very Brief About Me</h2>
+
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sequi voluptates
+      ducimus nulla sint dicta provident facere, deleniti temporibus repellat reprehenderit error
+      quidem explicabo suscipit quam. Veniam illum non deleniti!
+    </p>
+  </section>
+
+  <section>
+    <h2>Featured Portfolio Project(s)</h2>
+  </section>
+
+  <section>
+    <h2>Contact Me</h2>
+  </section>
 </main>
