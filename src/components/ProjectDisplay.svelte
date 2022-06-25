c645cf4b2067f555f8eb2c435860ba036e238dde
nbur4556 <nbur4556@gmail.com>
2022-06-25 16:50:34 -0500

    style: content heiarchy and spacing

--- src/components/ProjectDisplay.svelte
+++ src/components/ProjectDisplay.svelte
@@ -4,8 +4,8 @@
   export let item: Project;
 </script>
 
-<section>
-  <h3>{item.title}</h3>
+<section class="my-4">
+  <h3 class="text-lg font-bold">{item.title}</h3>
   <p>{item.description}</p>
   <p><a href={item.websiteLink} target="_blank">{item.websiteLink}</a></p>
   <p><a href={item.repoLink} target="_blank">{item.repoLink}</a></p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,16 +12,16 @@
     <h2 class=" text-2xl font-bold text-slate-100">I make things on the web</h2>
   </header>
 
-  <section class="flex-col p-4">
+  <section class="flex-col">
     <Card>
-      <p>
+      <p class="my-4">
         I am a full stack developer based in Austin Texas. I have a passion in creating beautiful
         and responsive web applications powered by scalable servers and databases.
       </p>
     </Card>
 
     <Card>
-      <h2>Featured Portfolio Project(s)</h2>
+      <h2 class="text-2xl font-bold">Featured Portfolio Project(s)</h2>
 
       {#each $projects as project, index}
         <ProjectDisplay item={project} />
@@ -29,7 +29,7 @@
     </Card>
 
     <Card>
-      <h2>Work Experience</h2>
+      <h2 class="text-2xl font-bold">Work Experience</h2>
 
       <WorkExperience title="Melodix">
         Developing a full-stack web application for local musicians.
@@ -37,17 +37,9 @@
     </Card>
 
     <Card>
-      <h2>Work Experience</h2>
+      <h2 class="text-2xl font-bold">Contact Me</h2>
 
-      <WorkExperience title="Melodix">
-        Developing a full-stack web application for local musicians.
-      </WorkExperience>
-    </Card>
-
-    <Card>
-      <h2>Contact Me</h2>
-
-      <ul>
+      <ul class="my-4">
         <li><a href="mailto: nbur4556@gmail.com" target="_blank">nbur4556@gmail.com</a></li>
         <li><a href="https://github.com/nbur4556" target="_blank">Github</a></li>
         <li><a href="https://www.linkedin.com/in/nick-burt/" target="_blank">Linked In</a></li>
