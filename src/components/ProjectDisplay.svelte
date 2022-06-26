ed56b8f345668bcc38a16542d2d1a70cd86bb87d
nbur4556 <nbur4556@gmail.com>
2022-06-26 16:29:22 -0500

    feat: add and display featured project images

--- src/components/ProjectDisplay.svelte
+++ src/components/ProjectDisplay.svelte
@@ -9,6 +9,8 @@
   <h3 class="text-lg font-bold">{item.title}</h3>
   <p>{item.description}</p>
 
+  <img src={item.image} alt={item.imageText} />
+
   <Link href={item.websiteLink} external>website</Link>
   <Link href={item.repoLink} external>repository</Link>
 </section>
