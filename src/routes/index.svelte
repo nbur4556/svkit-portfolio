167fc46cc1f21f7aca48d6f79b289a4f38c80bed
nbur4556 <nbur4556@gmail.com>
2022-06-26 18:53:58 -0500

    test: vercel preview message

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,25 +7,5 @@
 </script>
 
 <main class="flex flex-col items-center space-y-48">
-  <Hero />
-
-  <FeaturedProjects />
-
-  <Card cardClass="w-9/12" headingClass="py-4 px-10" contentClass="p-10">
-    <h2 slot="heading" class="text-2xl font-bold text-slate-100">Work Experience</h2>
-
-    <WorkExperience title="Melodix">
-      Developing a full-stack web application for local musicians.
-    </WorkExperience>
-  </Card>
-
-  <Card cardClass="w-9/12" headingClass="py-4 px-10" contentClass="p-10">
-    <h2 slot="heading" class="text-2xl font-bold text-slate-100">Contact Me</h2>
-
-    <ul class="my-4">
-      <li><Link href="mailto: nbur4556@gmail.com" external>nbur4556@gmail.com</Link></li>
-      <li><Link href="https://github.com/nbur4556" external /></li>
-      <li><Link href="https://www.linkedin.com/in/nick-burt/" external /></li>
-    </ul>
-  </Card>
+  <h1>Hello Vercel</h1>
 </main>
