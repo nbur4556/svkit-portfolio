64d7df8fd3c0f78ee1b0673d29c78fac30e73531
nbur4556 <nbur4556@gmail.com>
2022-06-27 15:47:55 -0500

    test: confirm functionality for project section of landing page

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,18 +8,18 @@
 
 <section class={`rounded overflow-hidden ${cardClass}`} data-cy={testId}>
   {#if $$slots.heading}
-    <div class={`bg-slate-800 ${headingClass}`}>
+    <div class={`bg-slate-800 ${headingClass}`} data-cy={`${testId}-heading`}>
       <slot name="heading" />
     </div>
   {/if}
 
   {#if $$slots.image}
-    <div class={`bg-slate-200 ${imageClass}`}>
+    <div class={`bg-slate-200 ${imageClass}`} data-cy={`${testId}-image`}>
       <slot name="image" />
     </div>
   {/if}
 
-  <div class={`bg-slate-200 ${contentClass}`}>
+  <div class={`bg-slate-200 ${contentClass}`} data-cy={`${testId}-content`}>
     <slot />
   </div>
 </section>
