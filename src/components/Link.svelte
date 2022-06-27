a0619f9efc5dc2d64d95e57b7698827624950dab
nbur4556 <nbur4556@gmail.com>
2022-06-27 14:44:21 -0500

    test: navbar functionality

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -2,11 +2,12 @@
   export let href: string;
   export let external: boolean = false;
   export let styleClass: string = "text-sky-900 hover:text-sky-600";
+  export let testId: string = "";
 
   $: targetAttribute = external ? "_blank" : null;
 </script>
 
-<a class={styleClass} {href} target={targetAttribute}>
+<a class={styleClass} {href} target={targetAttribute} data-cy={testId}>
   {#if $$slots.default}
     <slot />
   {:else}
