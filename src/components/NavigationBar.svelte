1cbef33e75008d9d8903664f99510b15ccff4369
nbur4556 <nbur4556@gmail.com>
2022-06-27 12:25:09 -0500

    feat: navigation bar uses an array of navigation routes to create links

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -1,17 +1,25 @@
-<script>
+<script lang="ts">
+  import Link from "./Link.svelte";
   import { paths } from "../util/paths";
+
+  interface NavigationRoute {
+    href: string;
+    linkText: string;
+  }
+
+  const navigationRoutes: Array<NavigationRoute> = [
+    { href: paths.featured, linkText: "Featured Projects" },
+    { href: paths.experience, linkText: "Work Experience" },
+    { href: paths.contact, linkText: "Contact Me" },
+  ];
 </script>
 
 <nav>
   <ul>
-    <li>
-      <a href={paths.featured}>Featured Projects</a>
-    </li>
-    <li>
-      <a href={paths.experience}>Work Experience</a>
-    </li>
-    <li>
-      <a href={paths.contact}>Contact Me</a>
-    </li>
+    {#each navigationRoutes as route}
+      <li>
+        <Link href={route.href}>{route.linkText}</Link>
+      </li>
+    {/each}
   </ul>
 </nav>
