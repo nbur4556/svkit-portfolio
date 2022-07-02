5165126cb37d48d74285979609bcf251fc1ff805
nbur4556 <nbur4556@gmail.com>
2022-07-02 18:00:58 -0500

    style: create and apply responsive page section component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { onDestroy } from "svelte";
 
-  import Card from "../components/Card.svelte";
+  import Contact from "../components/Contact.svelte";
   import FeaturedProjects from "../components/FeaturedProjects.svelte";
   import Hero from "../components/Hero.svelte";
-  import Link from "../components/Link.svelte";
   import NavigationBar from "../components/NavigationBar.svelte";
   import PageContent from "../components/PageContent.svelte";
+  import PageSection from "../components/PageSection.svelte";
   import WorkExperienceList from "../components/WorkExperienceList.svelte";
   import { projectStore } from "../stores/projectStore";
   import { experienceStore } from "../stores/experienceStore";
@@ -35,17 +35,14 @@
   <NavigationBar />
   <PageContent>
     <Hero />
-    <FeaturedProjects {projectData} />
-    <WorkExperienceList {experienceData} />
-
-    <Card cardClass="w-7/12" headingClass="py-4 px-10" contentClass="p-10" testId="contact">
-      <h2 slot="heading" id="contact" class="text-2xl font-bold text-slate-100">Contact Me</h2>
-
-      <ul class="my-4">
-        <li><Link href="mailto: nbur4556@gmail.com" external>nbur4556@gmail.com</Link></li>
-        <li><Link href="https://github.com/nbur4556" external /></li>
-        <li><Link href="https://www.linkedin.com/in/nick-burt/" external /></li>
-      </ul>
-    </Card>
+    <PageSection>
+      <FeaturedProjects {projectData} />
+    </PageSection>
+    <PageSection>
+      <WorkExperienceList {experienceData} />
+    </PageSection>
+    <PageSection>
+      <Contact />
+    </PageSection>
   </PageContent>
 </main>
