b0c757c1712c126fe1b3d047bc5ba36228060764
nbur4556 <nbur4556@gmail.com>
2023-05-02 10:09:55 -0500

    fix: migrate to new sveltekit routing

--- src/routes/projects/[id]/+page.svelte
+++ src/routes/projects/[id]/+page.svelte
@@ -1,5 +1,7 @@
 <!-- //TODO: Include when I have case studies. -->
 <script lang="ts">
+  throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
+
   export let id: string;
 </script>
 
