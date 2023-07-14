97845208989c9c9a288496bbd268616b7080ecb5
nbur4556 <nbur4556@gmail.com>
2023-07-14 17:10:34 -0500

    display logo in container in hero component

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,6 +2,9 @@
   class="h-screen w-screen max-w-screen-xl flex flex-col justify-center items-center self-center lg:px-10 lg:items-end"
   data-cy="hero"
 >
-  <h1 class="p-4 text-5xl text-center lg:text-right text-slate-100">I'm Nick Burt</h1>
+  <!-- <h1 class="p-4 text-5xl text-center lg:text-right text-slate-100">I'm Nick Burt</h1> -->
+  <div class="flex align-middle h-36">
+    <img class="object-cover" src="/images/logo.png" alt="Nick Burt" />
+  </div>
   <h2 class="p-4 text-4xl text-center lg:text-right text-slate-400">I make things for the web</h2>
 </section>
