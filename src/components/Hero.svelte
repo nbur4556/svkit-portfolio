456c74ea2daaeb307bdbe48e829ac93e5c42f266
nbur4556 <nbur4556@gmail.com>
2023-07-14 17:22:25 -0500

    adjust logo alignment within hero component

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,9 +2,9 @@
   class="h-screen w-screen max-w-screen-xl flex flex-col justify-center items-center self-center lg:px-10 lg:items-end"
   data-cy="hero"
 >
-  <!-- <h1 class="p-4 text-5xl text-center lg:text-right text-slate-100">I'm Nick Burt</h1> -->
-  <div class="flex align-middle h-36">
-    <img class="object-cover" src="/images/logo.png" alt="Nick Burt" />
+  <!-- //TODO: Select final logo -->
+  <div class="flex align-middle justify-end h-36">
+    <img class="object-contain w-3/4" src="/images/logo.png" alt="Nick Burt" />
   </div>
   <h2 class="p-4 text-4xl text-center lg:text-right text-slate-400">I make things for the web</h2>
 </section>
