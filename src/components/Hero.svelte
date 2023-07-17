663e0bb974fdb0d62b97b2b1cedcb645b4382e18
nbur4556 <nbur4556@gmail.com>
2023-07-17 18:07:12 -0500

    responsive styles for logo

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,8 +2,8 @@
   class="h-screen w-screen max-w-screen-xl flex flex-col justify-center items-center self-center lg:px-10 lg:items-end"
   data-cy="hero"
 >
-  <div class="flex align-middle justify-end h-36">
-    <img class="object-contain w-1/2" src="/images/full-logo.png" alt="Nick Burt" />
+  <div class="flex align-middle justify-center lg:justify-end">
+    <img class="px-4 object-contain lg:w-1/2" src="/images/full-logo.png" alt="Nick Burt" />
   </div>
   <h2 class="p-4 text-4xl text-center lg:text-right text-slate-400">I make things for the web</h2>
 </section>
