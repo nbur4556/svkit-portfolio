33f1307642bc1ad3af22e51417f0e1600fcf61fa
nbur4556 <nbur4556@gmail.com>
2023-07-24 19:43:46 -0500

    align project data with individual project images

--- src/components/FeaturedProjects.svelte
+++ src/components/FeaturedProjects.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import ImageContainer from "./UI/ImageContainer.svelte";
   import Link from "./Link.svelte";
-  import ProjectPreview from "./ProjectPreview.svelte";
 
   import type { Project } from "../stores/projectStore";
 
@@ -13,15 +12,18 @@
 <section data-cy="select-feature">
   <h2 id="featured" class="mb-74px text-2xl text-white font-bold">{"> My Projects"}</h2>
 
-  <section class="flex flex-row items-stretch gap-32px h-442px">
-    <ImageContainer classExt="rounded-2xl" src={selected.image} alt={selected.imageText} />
-    <ImageContainer classExt="rounded-2xl" src={selected.image} alt={selected.imageText} />
-    <ImageContainer classExt="rounded-2xl" src={selected.image} alt={selected.imageText} />
+  <section class="flex flex-row items-stretch gap-32px">
+    <div>
+      <ImageContainer classExt="rounded-2xl h-442px" src={selected.image} alt={selected.imageText} />
+      <Link href={selected.websiteLink} external testId="select-website">Website</Link>
+    </div>
+    <div>
+      <ImageContainer classExt="rounded-2xl h-442px" src={selected.image} alt={selected.imageText} />
+      <p>{selected.description}</p>
+    </div>
+    <div>
+      <ImageContainer classExt="rounded-2xl h-442px" src={selected.image} alt={selected.imageText} />
+      <Link href={selected.repoLink} external testId="select-repository">Repository</Link>
+    </div>
   </section>
-
-  <p>{selected.description}</p>
-  <div class="flex flex-row space-x-10">
-    <Link href={selected.websiteLink} external testId="select-website">Website</Link>
-    <Link href={selected.repoLink} external testId="select-repository">Repository</Link>
-  </div>
 </section>
