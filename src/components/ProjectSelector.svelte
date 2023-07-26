3601980a178882d9429419ee7bd1f0b729f7b766
nbur4556 <nbur4556@gmail.com>
2023-07-25 19:02:05 -0500

    add optional arrow button controls

--- src/components/ProjectSelector.svelte
+++ src/components/ProjectSelector.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
 
+  import Chevron from "../components/SVG/Chevron.svelte";
+
   export let count: number;
 
   const dispatch = createEventDispatcher();
 
   let selected: number = 1;
+
+  const stepSelected = (step: number) => {
+    if(selected + step > 0 && selected + step <= count) {
+      selected += step;
+      handleUpdate();
+    }
+  }
 
   const handleUpdate = () => {
     dispatch("update", {
@@ -17,7 +26,7 @@
   $: displayCount = count < 10 ? `0${count}` : count;
 </script>
 
-<section class="flex flex-col items-center">
+<section class="flex flex-col items-center gap-xxs">
   <input
     class="appearance-none h-4px w-xl bg-body accent accent-accent1-400 overflow-hidden cursor-pointer"
     type="range"
@@ -26,5 +35,9 @@
     max={count}
     on:change={handleUpdate}
   />
-  <p class="text-body">{displaySelected} / {displayCount}</p>
+  <div class="flex flex-row items-center gap-xs">
+    <button on:click={() => stepSelected(-1)}><Chevron fill="#0FFEFF" direction={-1} /></button>
+    <p class="text-body">{displaySelected} / {displayCount}</p>
+    <button on:click={() => stepSelected(1)}><Chevron fill="#0FFEFF" /></button>
+  </div>
 </section>
