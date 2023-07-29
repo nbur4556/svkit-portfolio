1157c6c5802fd89497feca2d94efab70b15674a3
nbur4556 <nbur4556@gmail.com>
2023-07-29 12:31:55 -0500

    fade color over featured project images with text content

--- src/components/FeaturedProjectItem.svelte
+++ src/components/FeaturedProjectItem.svelte
@@ -2,17 +2,24 @@
   export let image: string;
   export let title: string | undefined = undefined;
   export let description: string | undefined = undefined;
+
+  $: textContent = description !== undefined || title !== undefined;
 </script>
 
-<!-- //TODO: faded image when text is visible -->
-<div
-  class="flex flex-col justify-center w-full p-md h-440px bg-cover bg-center rounded-2xl"
+<section
+  class="w-full h-440px bg-cover bg-center rounded-2xl overflow-hidden"
   style={`background-image:url(${image})`}
 >
-  {#if title}
-    <p class="text-lg text-white text-center p-xs">{title}</p>
-  {/if}
-  {#if description}
-    <p class="max-w-xl text-xs text-white">{description}</p>
-  {/if}
-</div>
+  <div
+    class={`flex flex-col justify-center p-md w-full h-full ${
+      textContent ? "bg-dark bg-opacity-90" : null
+    }`}
+  >
+    {#if title}
+      <p class="text-lg text-white text-center p-xs">{title}</p>
+    {/if}
+    {#if description}
+      <p class="max-w-xl text-xs text-white">{description}</p>
+    {/if}
+  </div>
+</section>
