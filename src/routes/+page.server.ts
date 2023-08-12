5933ab9f1b24ab7d1ee53d96c8b3092913cb98fd
nbur4556 <nbur4556@gmail.com>
2023-08-12 17:20:25 -0500

    format contact form email

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,11 @@
 import * as nodemailer from "nodemailer";
 import { FORWARDMAIL_USER, FORWARDMAIL_PASS} from "$env/static/private";
 
-//TODO: nodemailer typing
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    //TODO: These should be environment variables
     user: FORWARDMAIL_USER,
     pass: FORWARDMAIL_PASS
   }
@@ -17,12 +15,22 @@ export const actions = {
   default: async ({request}) => {
     const formData = await request.formData();
 
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const phone = formData.get("phone");
+    const message = formData.get("message");
+
     try {
       const info = await transporter.sendMail({
-        //TODO: These should be environment variables
         to: "nbur4556@gmail.com",
         subject: "Portfolio Message",
-        text: `Name: ${formData.get("name")}, email: ${formData.get("email")}, phone: ${formData.get("phone")}, message: ${formData.get("message")}`,
+        text: ` ${message}
+      
+        Email: ${email}
+        Phone Number: ${phone}
+
+        From ${name}
+        `,
       });
   
       console.log(`Message sent: ${info.messageId}`)
