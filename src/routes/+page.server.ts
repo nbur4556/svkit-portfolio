eaa83e14d07dcd34ea6e59699ba199ac5bbaff96
nbur4556 <nbur4556@gmail.com>
2023-08-12 16:48:53 -0500

    create initial message transporter

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,28 @@
+import * as nodemailer from "nodemailer";
+
+//TODO: nodemailer typing
+const transporter = nodemailer.createTransport({
+  host: "smtp.forwardemail.net",
+  port: 465,
+  secure: true,
+  auth: {
+    //TODO: These should be environment variables
+    user: "USERNAME",
+    pass: "PASSWORD"
+  }
+})
+
 export const actions = {
   default: async ({request}) => {
     const formData = await request.formData();
-    console.log(formData.get("name"));
-    console.log(formData.get("email"));
-    console.log(formData.get("phone"));
-    console.log(formData.get("message"));
-  }
+
+    const info = await transporter.sendMail({
+      //TODO: These should be environment variables
+      to: "nbur4556@gmail.com",
+      subject: "Portfolio Message",
+      text: `Name: ${formData.get("name")}, email: ${formData.get("email")}, phone: ${formData.get("phone")}, message: ${formData.get("message")}`,
+    });
+
+    console.log(`Message sent: ${info.messageId}`)
+  },
 };
