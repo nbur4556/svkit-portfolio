36c84fdbe987bef6fd7c090669dbb7e0ed1fc62a
Nick Burt <nbur4556@gmail.com>
2026-07-01 18:00:18 -0500

    adjust colors of hero image to pop from and blend with background

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -13,7 +13,6 @@
   <div>
     <!--TODO: graphics-update: I should attribute unDraw if they want it, even if not required -->
     <!--TODO: graphics-update: Resize image -->
-    <!--TODO: graphics-update: Image should have a white edge, or otherwise pop from the website background-->
     <!--TODO: graphics-update: formatting for mobile -->
     <img
       class="w-688px"
